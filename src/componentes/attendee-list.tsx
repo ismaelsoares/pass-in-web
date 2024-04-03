77842f84ff29b32143324c8f9e85a963af5e3f67
Ismael Soares <ismaelsoaresdoria@gmail.com>
2024-04-03 14:53:01 -0300

    Finish part 1

--- src/componentes/attendee-list.tsx
+++ src/componentes/attendee-list.tsx
@@ -1,4 +1,4 @@
-import { MoreHorizontal, Search } from 'lucide-react'
+import { ChevronLeft, ChevronRight, ChevronsLeft, ChevronsRight, MoreHorizontal, Search } from 'lucide-react'
 
 export const AttendeeList = () => {
   return (
@@ -8,7 +8,7 @@ export const AttendeeList = () => {
         <div className="px-3 py-1.5 border w-72 border-white/10 items-center text-sm rounded-lg gap-3 flex">
           <Search className='size-4 text-emerald-300' />
           <input
-            className="flex-1 bg-transparent outline-none"
+            className="flex-1 p-0 text-sm bg-transparent border-0 outline-none"
             type="text"
             placeholder="Buscar participante..."
           />
@@ -20,7 +20,7 @@ export const AttendeeList = () => {
           <thead>
             <tr className='border-b border-white/10 '>
               <th style={{ width: 48 }} className='px-4 py-3 text-sm font-semibold text-left'>
-                <input type="checkbox" />
+                <input type="checkbox" className='border-white/10 size-4 bg-black/20' />
               </th>
               <th className='px-4 py-3 text-sm font-semibold text-left'>Código</th>
               <th className='px-4 py-3 text-sm font-semibold text-left'>Participante</th>
@@ -32,9 +32,9 @@ export const AttendeeList = () => {
           <tbody>
             {Array.from({ length: 8 }).map((_, i) => {
               return (
-                <tr key={i} className='border-b border-white/10'>
+                <tr key={i} className='border-b border-white/10 hover:bg-white/5'>
                   <td className='px-4 py-3 text-sm text-zinc-300'>
-                    <input type="checkbox" />
+                    <input type="checkbox" className='border-white/10 size-4 bg-black/20' />
                   </td>
                   <td className='px-4 py-3 text-sm text-zinc-300'>123285</td>
                   <td className='px-4 py-3 text-sm text-zinc-300'>
@@ -60,7 +60,23 @@ export const AttendeeList = () => {
                 Mostrando 10 de 228 itens
               </td>
               <td className='px-4 py-3 text-sm text-right text-zinc-300' colSpan={3}>
-                Página 1 de 23
+                <div className='inline-flex items-center gap-8'>
+                  <span>Página 1 de 23</span>
+                  <div className='flex gap-1.5'>
+                    <button className='border rounded-md border-white/10 bg-white/10 p-1.5'>
+                      <ChevronsLeft className='size-4' />
+                    </button>
+                    <button className='border rounded-md border-white/10 bg-white/10 p-1.5'>
+                      <ChevronLeft className='size-4' />
+                    </button>
+                    <button className='border rounded-md border-white/10 bg-white/10 p-1.5'>
+                      <ChevronRight className='size-4' />
+                    </button>
+                    <button className='border rounded-md border-white/10 bg-white/10 p-1.5'>
+                      <ChevronsRight className='size-4' />
+                    </button>
+                  </div>
+                </div>
               </td>
             </tr>
           </tfoot>
