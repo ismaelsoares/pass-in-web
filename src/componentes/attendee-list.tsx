d58948bbcb510ce0af4fdf14dcd43ec9d24b503d
Ismael Soares <ismaelsoaresdoria@gmail.com>
2024-04-03 09:56:37 -0300

    Style header and including inputs

--- src/componentes/attendee-list.tsx
+++ src/componentes/attendee-list.tsx
@@ -1,5 +1,12 @@
 export const AttendeeList = () => {
-    return (
-        <div>AttendeeList</div>
-    )
+  return (
+    <div className="flex items-center gap-3">
+      <div className="text-2xl font-bold">Participantes</div>
+      <input
+        className="px-3 py-1.5 border w-72 border-white/10 bg-transparent text-sm rounded-lg"
+        type="text"
+        placeholder="Buscar participante..."
+      />
+    </div>
+  )
 }
